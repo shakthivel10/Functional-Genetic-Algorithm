39e3f7986fecb18b7d544346ee50cb536efcef83
shakthivel <shakthivels2803@gmail.com>
2020-11-28 23:47:27 -0500

    Add util functions

--- GA.hs
+++ GA.hs
@@ -13,3 +13,17 @@ type Count = Integer
 
 type Size = Integer 
 
+getChromosome :: Evaluated c -> c
+getChromosome (f, c) = c
+
+getFitness :: Evaluated c -> FitnessValue
+getFitness (f, c) = f
+
+compareChromosomes :: Evaluated c -> Evaluated c -> Evaluated c
+compareChromosomes (f1, c1) (f2, c2) | f1 <= f2 =  (f1, c1)
+                                     | otherwise = (f2, c2)
+
+getChromosomeWithMinFitness :: Population a ->  Evaluated a
+getChromosomeWithMinFitness [] = error "Error, Empty Population."
+getChromosomeWithMinFitness p = head (filter ((==) minVal . fst) p) where minVal = minimum (map fst p)
+
