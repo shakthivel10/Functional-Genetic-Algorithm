e9f3a13ae1a2c911f16e6f66a298278454a4575d
shakthivel <shakthivels2803@gmail.com>
2020-11-29 23:38:48 -0500

    Add BinPacking Evolution Code

--- BinPacking.hs
+++ BinPacking.hs
@@ -31,3 +31,21 @@ totalWeightsInBinsHelper currentIndex n w b | currentIndex == n = []
 
 sumOfWeightsInCurrentIndex :: Int -> Weights -> BinPackChromosome -> Int
 sumOfWeightsInCurrentIndex currentIndex w b = fromInteger(sum (zipWith (*) (map (\x -> if x == currentIndex then 1 else 0) b) w))
+
+binCrossOver :: Seed -> BinPackChromosome -> BinPackChromosome -> BinPackChromosome
+binCrossOver s parent1 parent2 = let k = randomNumberLessthanN s (length parent1) in ((take k parent1) ++ reverse (take ((length parent1)-k) (reverse parent2)))
+
+binpackga :: NumberOfBins -> Weights -> Seed -> Count -> Population BinPackChromosome -> Evaluated BinPackChromosome
+binpackga n w s 0 p =  getChromosomeWithMinFitness p
+binpackga n w s c p | fromIntegral (getFitness (getChromosomeWithMinFitness p)) <= 0.05 * fromIntegral (sum w) = getChromosomeWithMinFitness p
+                    | otherwise = binpackga n w (randomNumberLessthanN s 1000) (c-1) (binEvolution n w s p)
+
+binEvolution :: NumberOfBins -> Weights -> Seed -> Population BinPackChromosome -> Population BinPackChromosome
+binEvolution n w s p =  binEvolutionHelper n w (fromIntegral (length p)) s p
+
+binEvolutionHelper :: NumberOfBins -> Weights -> Count -> Seed -> Population BinPackChromosome -> Population BinPackChromosome
+binEvolutionHelper n w 0 s p = [] 
+binEvolutionHelper n w c s p = let childChromosome = (binCrossOver s (getChromosome ( p!! (randomNumberLessthanN (randomNumberLessthanN (s+99) 99) (length p)) )) (getChromosome (p!!(randomNumberLessthanN (randomNumberLessthanN s 100) (length p)))) ) in ((binFitness n w childChromosome), childChromosome) : (binEvolutionHelper n w (c-1) (randomNumberLessthanN (s+(fromIntegral c)) 100) p)
+
+binPackGASolve :: NumberOfBins -> Weights -> Seed -> Count -> Size -> Evaluated BinPackChromosome
+binPackGASolve n w seed c size = binpackga n w seed c (binmakepop n w seed size)
