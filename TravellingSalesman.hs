59c96e17624e73e0aecef86b0774edb8ccf151ca
shakthivel <shakthivels2803@gmail.com>
2020-12-01 15:33:55 -0500

    Add TSP Fitness Function

--- TravellingSalesman.hs
+++ TravellingSalesman.hs
@@ -27,4 +27,14 @@ insertBetweenListHelper a [] (x:xs)     = (a:(x:xs)) : (insertBetweenListHelper 
 insertBetweenListHelper a (x:xs) (y:ys) = ((x:xs) ++ [a] ++ (y:ys)) : (insertBetweenListHelper a ((x:xs) ++ [y]) ys)
 insertBetweenListHelper a (x:xs) []     =  [((x:xs) ++ [a])]
 
+tspFitness :: TSPChromosome -> FitnessValue
+tspFitness c =   ( tspFitnessHelper c ) + (euclidianDistance (head c) (head (reverse c))) -- adding distance between first and last nodes
+
+tspFitnessHelper :: TSPChromosome -> FitnessValue
+tspFitnessHelper [] = 0
+tspFitnessHelper (x:[]) = 0
+tspFitnessHelper (x:xs) = (euclidianDistance x (head xs)) + tspFitnessHelper (xs)
+
+euclidianDistance :: Coordinate -> Coordinate -> Int 
+euclidianDistance  (x1,y1) (x2,y2) =  floor (sqrt (fromIntegral ((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2))))
 
